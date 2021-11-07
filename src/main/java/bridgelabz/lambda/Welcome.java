3b364371b6ae6665b62945c36ac65e84f015bcc4
manepriyanka2014 <manepriyanka2014@gmail.com>
2021-11-07 15:40:57 +0530

    Added Lambda user registration

---
+++ src/main/java/bridgelabz/lambda/Welcome.java
@@ -0,0 +1,7 @@
+package bridgelabz.lambda;
+
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Lambda User Registration");
+	}
+}
---
+++ src/test/java/bridgelabz/lambda/ExceptionUserRegistrationTest.java
@@ -0,0 +1,24 @@
+package bridgelabz.lambda;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExceptionUserRegistrationTest {
+
+    ExceptionUserRegistration exceptionUserRegistration = new ExceptionUserRegistration();
+
+    @Test
+    public void givenFirstName_IsProper_ReturnTrue() {
+        boolean actualResult = exceptionUserRegistration.firstName("Ramakrishna");
+        Assert.assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void givenFirstName_IsProper_ReturnFalse() {
+        boolean actualResult = exceptionUserRegistration.firstName("RamaKrishna");
+        Assert.assertEquals(false, actualResult);
+    }
+
+}
